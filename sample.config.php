27bc3ac5799585810fc99b48489548edfc32c32f
andris9 <andris@kreata.ee>
2011-03-19 08:30:44 +0200

    post initial

---
+++ sample.config.php
@@ -0,0 +1,17 @@
+<?php
+
+//Andmebaasi konstandid
+define ("MYSQL_SERVER_NAME", "localhost"); 
+define ("MYSQL_SERVER_USER", "db_username"); 
+define ("MYSQL_SERVER_PASS", "db_password"); 
+define ("MYSQL_SERVER_BASE", "db_base"); 
+
+//Andmebaasiga ühendamine
+$dbconnect=mysql_connect(MYSQL_SERVER_NAME,MYSQL_SERVER_USER,MYSQL_SERVER_PASS) or die("1:".mysql_error()); 
+mysql_select_db(MYSQL_SERVER_BASE) or die("2:".mysql_error());
+
+// Lokaal ja tähetabelid
+mysql_set_charset('utf8');
+mb_internal_encoding("UTF-8");
+mb_regex_encoding("UTF-8");
+setlocale(LC_ALL,'et_EE.UTF-8');
